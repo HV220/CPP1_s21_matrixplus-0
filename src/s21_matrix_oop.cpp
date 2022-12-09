31d6bbf582cd19ee54b16b55b5e22a25292cbe41
HV_220 <diagenin@gmail.com>
2022-12-09 13:51:09 +0300

    dev
    dev1.2
    
    problem with determinant

--- src/s21_matrix_oop.cpp
+++ src/s21_matrix_oop.cpp
@@ -4,15 +4,18 @@ using namespace src;
 
 int main()
 {
-   S21Matrix test = S21Matrix(2, 3);
-   S21Matrix test1 = S21Matrix(2, 3);
+   S21Matrix *test = new S21Matrix(3, 3);
+   S21Matrix test1 = S21Matrix(3, 3);
    test1.ChargeMatrix();
-   test.ChargeMatrix();
+   test = new S21Matrix();
 
-   test.SumMatrix(test1);
-
-   test.PrintMatrix();
-   test.SubMatrix(test1);
-   test.PrintMatrix();
    return 0;
 }
+
+// test.SumMatrix(test);
+// test.PrintMatrix();
+// test1.PrintMatrix();
+// // test.PrintMatrix();
+// test.MulMatrix(test1);
+// test.PrintMatrix();
+// // test.PrintMatrix();
--- src/s21_matrix_oop.h
+++ src/s21_matrix_oop.h
@@ -158,10 +158,113 @@ namespace src
         }
       }
     }
-    void MulMatrix(const S21Matrix &other);
-    S21Matrix Transpose();
-    S21Matrix CalcComplements();
-    double Determinant();
+
+    void MulMatrix(const S21Matrix &other)
+    {
+
+      if (!this->EmptyMatrix(other))
+      {
+        return;
+      }
+
+      if (this->cols_ != other.rows_)
+      {
+        return;
+      }
+
+      S21Matrix *result = new S21Matrix(this->rows_, other.cols_);
+
+      if (!result->GetMatrix())
+      {
+        return;
+      }
+
+      int tmp = 0;
+      int tmp_columns = 0;
+
+      for (int i = 0; i < result->rows_; i++, tmp++)
+      {
+        for (int j = 0; j < result->cols_; j++)
+        {
+          for (int k = 0; k < this->cols_; k++)
+          {
+            result->matrix_[i][j] += (this->matrix_[tmp][k]) *
+                                     (other.matrix_[k][tmp_columns]);
+          }
+          tmp_columns++;
+        }
+        tmp_columns = 0;
+      }
+
+      this->cols_ = result->cols_;
+      this->rows_ = result->rows_;
+      this->RealisationCalloc();
+      this->CopyMatrix(*result, this->rows_, this->cols_);
+      delete (result);
+    }
+
+    S21Matrix Transpose()
+    {
+
+      S21Matrix *result = new S21Matrix(this->cols_, this->rows_);
+
+      int tmp = 0;
+
+      for (int i = 0; i < result->rows_; i++)
+      {
+        for (int j = 0; j < result->cols_; j++)
+        {
+          result->matrix_[i][j] = this->matrix_[j][tmp];
+        }
+        tmp++;
+      }
+
+      return *result;
+    }
+
+    S21Matrix CalcComplements()
+    {
+    }
+
+    // double Determinant()
+    // {
+    //   double tmp = 0.0;
+
+    //   if (!(this->rows_ != this->cols_) || !(this->EmptyMatrix()))
+    //   {
+    //     return NULL;
+    //   }
+
+    //   if (this->rows_ == 1)
+    //   {
+    //     tmp = this->matrix_[0][0];
+    //   }
+
+    //   if (this->rows_ == 2)
+    //   {
+    //     tmp = (this->matrix_[0][0] * this->matrix_[1][1]) -
+    //           (this->matrix_[1][0] * this->matrix_[0][1]);
+    //   }
+
+    //   if (this->rows_ > 2)
+    //   {
+    //     S21Matrix *matrix = new S21Matrix();
+
+    //     for (int i = 0; i < this->rows_; i++)
+    //     {
+    //       if (this->EqMatrixDeterminant(matrix, 0, i))
+    //       {
+    //         break;
+    //       }
+    //       matrix->Determinant(result);
+    //       tmp += A->matrix[i][0] * pow(-1, i) * *result;
+    //       s21_remove_matrix(&matrix);
+    //     }
+    //   }
+    //   *result = tmp;
+    //   return error;
+    // }
+
     S21Matrix InverseMatrix();
 
     void PrintMatrix()
@@ -252,7 +355,42 @@ namespace src
 
       return status;
     }
-  };
 
+    int EmptyMatrix()
+    {
+      int status = 0;
+
+      if ((this->matrix_ != NULL) && (this->cols_ > 0) && (this->rows_ > 0))
+      {
+        status = 1;
+      }
+
+      return status;
+    }
+
+    int EqMatrixDeterminant(const S21Matrix *other, int row, int column)
+    {
+      int status = 0;
+      other = new S21Matrix(this->rows_ - 1, this->cols_ - 1);
+
+      for (int i = 0, y = 0; i < this->rows_; i++)
+      {
+        if (i != column)
+        {
+          for (int j = 0, x = 0; j < this->rows_; j++)
+          {
+            if (j != row)
+            {
+              other->matrix_[y][x] = this->matrix_[i][j];
+              x++;
+            }
+          }
+          y++;
+        }
+      }
+      return status;
+    }
+  };
 }
+
 #endif //  SRC_S21_MATRIX_OOP_H_
